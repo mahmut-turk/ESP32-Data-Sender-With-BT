0ca4680be2a65c6ffdc67ae8017b386642b12615
Mahmut Turk <142899979+mahmut-turk@users.noreply.github.com>
2025-09-05 23:43:37 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,16 +1,16 @@
 #include <Arduino.h>
 #include "BluetoothSerial.h"
 
-BluetoothSerial Mahmut;
+BluetoothSerial Mahmut;                    // set the bluetooth device name as "Mahmut"
 
 void setup() {
 Serial.begin(115200);
 Mahmut.begin("ESP32_Mahmut");
-Serial.println("Bluetooth data started");
+Serial.println("Bluetooth data started");  // if you want, you can monitor the data on the Serial Monitor
 }
 
 void loop() {
-  int sensorValue = analogRead(32);
-  Mahmut.println(sensorValue);
-  delay(100);
+  int sensorValue = analogRead(32);   // reading analog data with GPIO32 of ESP32 
+  Mahmut.println(sensorValue);        // sending data with Bluetooth 
+  delay(100);                         //  reading in 100 milisecond intervals
 }
